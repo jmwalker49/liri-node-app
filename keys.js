60cc4f271e375857a1a2f5975364c955ea6f3b94
Jmwalker <Jmwalker>
2017-02-15 13:18:33 -0500

    initial git push

---
+++ keys.js
@@ -0,0 +1,10 @@
+
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'pBjUWf2v4eH3XwHIG0cfUvWKz',
+  consumer_secret: 'riIWwPtkgrjWJF0JH5RTsBOa3dM5dSTiQKgtmCLOltfRtBhquR',
+  access_token_key: '1205957779-BmLeIAfiQ6XVCTtuauH8cncGABITdtzCV0xCX8y',
+  access_token_secret: 'By0ip3CxjMYuAbKvicuDfTsNIzIFsDxF01diA8U3n7oTJ',
+}
+
